9780c81bf1b8c8c209afedcbe998a8653001cc73
Gabriel Ferreira <willamigabrief.souza@gmail.com>
2023-07-15 08:47:13 -0300

    Routes added

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,9 @@
 //a conexão com o db deve ser feita aqui, portanto a importação do .env
+//os routes devem ser usados aqui
+const router = require('./router');
 
 const express = require('express');
 
 const app = express();
 
-//os routes devem ser usados aqui
-
+module.exports = app; //será importado em server.
--- Backend/src/router.js
+++ Backend/src/router.js
@@ -1,1 +1,12 @@
-//
+//Importar os controlers
+const movieController = require('./controllers/movieController');
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/')//home //obs.: rota que retornará todos os documento por enquanto.
+router.get('/:id')//recebe um query paramn sendo o nome do filme pesquisado
+
+
+module.exports = router; //importar em app.js
