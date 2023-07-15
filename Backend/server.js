70fb11f16014088c8a282a1b2cac3180cb53a206
Gabriel Ferreira <willamigabrief.souza@gmail.com>
2023-07-15 08:56:16 -0300

    update

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,5 @@
+
+
 const app = require('./src/app')
 
 const PORT = 3000;
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,11 @@
 //a conexão com o db deve ser feita aqui, portanto a importação do .env
-
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+//const cors = require('cors');
 
 const app = express();
+app.use(express.json());
 
 //os routes devem ser usados aqui
 
